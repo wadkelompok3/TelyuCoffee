f2a0c0d5fcb188a5113c9b41f18388831df12b8f
farahhelmi <farahhelmi@student.telkomuniversity.ac.id>
2023-12-22 07:40:29 +0700

    coba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
                 <div class="flex lg:hidden">
                     <button type="button"
                         class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                        <span class="sr-only">Open main menu</span>
+                        <span class="sr-only">OPEN MAIN MENU</span>
                         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round"
